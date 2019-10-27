e9ec8ab42efbea2e2b70011c03cc594058c96a30
Artyom2510 <SlavaSatane4434343@gmail.com>
2019-10-27 17:29:22 +0300

    v001

--- js/common.js
+++ js/common.js
@@ -1,34 +1,29 @@
 $(function () {
 
 	// Бургер и меню
-	let popupMenu = $('.js-menu-popup');
+	let burgerMenu = $('.js-burger-menu');
 	let burger = $('.js-btn-burger');
 	burger.on('click', function() {
-		$(this).toggleClass('open close');
+		$(this).toggleClass('open');
+		burgerMenu.slideToggle(200);
 	});
 
-	popupMenu.switchPopup({
-		btnClass: 'js-btn-burger',
+	$('.js-manifest-btn').on('click', function() {
+		$('.js-manifest-hidden').slideToggle(200);
+	});
+	
+	let popupProgram = $('.popup-program');
+	popupProgram.switchPopup({
+		btnClass: 'js-tgl-program',
 		duration: 300,
+		overflow: true
 	});
 
-	// Слайдер брендов
-	$('.slider-brands').slick({
-		dots: false,
-		infinite: true,
-		arrows: false,
-		speed: 500,
-		slidesToShow: 5,
-		slidesToScroll: 5,
-		autoplay: true,
-		autoplaySpeed: 3000,
-	});
-
-	// Скролл наверх
-	$('.js-up').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 400);
+	let popupLk = $('.popup-lk');
+	popupLk.switchPopup({
+		btnClass: 'js-tgl-lk',
+		duration: 300,
+		overflow: true
 	});
 
 });
