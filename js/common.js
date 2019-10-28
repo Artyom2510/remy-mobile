ad3072e7bea44b05b9783dd54b007aa3155cbbc2
Artyom <taranenko@grechka.digital>
2019-10-28 15:42:50 +0300

    wait for Max

--- js/common.js
+++ js/common.js
@@ -10,6 +10,7 @@ $(function () {
 
 	$('.js-manifest-btn').on('click', function() {
 		$('.js-manifest-hidden').slideToggle(200);
+		$('.manifest-img__title').toggleClass('manifest-img__title_open');
 	});
 	
 	let popupProgram = $('.popup-program');
@@ -24,6 +25,23 @@ $(function () {
 		btnClass: 'js-tgl-lk',
 		duration: 300,
 		overflow: true
+	});
+
+	$('.js-click-anim').on('click', function() {
+		$('.regitstration__title').removeClass('regitstration__title_visible');
+		$('.regitstration__wrap_1').addClass('regitstration__wrap_transform');
+		setTimeout(function () {
+			$('.regitstration__wrap_2').addClass('regitstration__wrap_transform-start');
+			$('.regitstration__wrap').toggleClass('display');
+		}, 300);
+		setTimeout(function () {
+			$('.regitstration__title').addClass('regitstration__title_visible');
+			$('.regitstration__wrap_2').removeClass('regitstration__wrap_transform-start');
+		}, 600);
+	});
+
+	$('.js-reg').on('click', function() {
+		$('.page').addClass('page_transform');
 	});
 
 });
